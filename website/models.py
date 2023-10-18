aab1d8fdb331431124cf8229acc90acb1b01d073
Sidhant-yadav-BC <sidhant.yadav@blenheimchalcot.com>
2023-10-18 12:38:33 +0530

    changes database schema

--- website/models.py
+++ website/models.py
@@ -2,31 +2,57 @@ from . import db
 from flask_login import UserMixin
 
 
+
 class BusinessUpdates(db.Model):
-    update_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime(timezone=False))
-    username= db.Column(db.String(200), nullable= False)
-    user_input=db.Column(db.String(10000))
-    user_output=db.Column(db.String(10000))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user_input = db.Column(db.String(10000))
+    user_output = db.Column(db.String(10000))
+    blockers = db.Column(db.String())
+    kpi = db.Column(db.String(500))
     service = db.Column(db.String(100))
-    portfolio = db.Column(db.String(200))
-    teammates = db.Column(db.String(1000), default=None)
-    progress = db.Column(db.String(100))
+    
+    # Foreign key references to Projects and Portfolios
+    project_id = db.Column(db.Integer, db.ForeignKey('projects.id'))
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolios.id'))
+    
+    teammates = db.Column(db.String(1000), default="")
+    status = db.Column(db.String(100))
     ai_input = db.Column(db.String(10000))
     ai_output = db.Column(db.String(10000))
-    business_update=db.Column(db.String(10000))
+    business_update = db.Column(db.String(10000))
+    
+class Portfolios(db.Model):
+    id = db.Column(db.Integer, primary_key = True, nullable = False, unique = True)
+    name = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.String())
+    
+    business_updates = db.relationship('BusinessUpdates')
+    projects = db.relationship('Projects')
+
+    
+class Projects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey('portfolios.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000))  # You can adjust the length as needed
+    
+    business_updates = db.relationship('BusinessUpdates')
 
 class Users(db.Model, UserMixin):
     
     def get_id(self):
-        return str(self.user_id)
+        return str(self.id)
     
     @property
     def id(self):
-        return self.user_id
+        return self.id
     
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     role = db.Column(db.String(50), nullable=False, default='user', server_default='user')
     username = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    
+    business_updates = db.relationship('BusinessUpdates')
